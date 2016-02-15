5da55fdfaa64a028fd9500d4c4b931e02c8ae3d4
Raphael Reitzig <reitzig@cs.uni-kl.de>
2016-02-15 16:27:20 +0100

    A plugin for using images in posts.

---
+++ _plugins/flair_img.rb
@@ -0,0 +1,22 @@
+## Liquid tag 'maincolumn-figure' used to add image data that fits within the
+## main column area of the layout
+## Usage {% maincolumn 'path/to/image' 'This is the caption' %}
+#
+module Jekyll
+  class FlairImageTag < Liquid::Tag
+
+  	require "shellwords"
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text.shellsplit
+    end
+
+    def render(context)
+      @text[1] = "" if @text[1].nil?
+      "<p class=\"flairimg\"><img src=\"#{@text[0]}\" alt=\"#{@text[1]}\" title=\"#{@text[1]}\"></img></p>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('flairimg', Jekyll::FlairImageTag)
